7856f625017948f5d375efaa93d2a776ec8b6f54
Nathan VILLARD <nathan.villard@etu.univ-amu.fr>
2026-06-01 09:23:24 +0000

    Exercice 1 terminé

--- src/main/java/fr/univ_amu/iut/exercice1/MessageViewModel.java
+++ src/main/java/fr/univ_amu/iut/exercice1/MessageViewModel.java
@@ -1,5 +1,6 @@
 package fr.univ_amu.iut.exercice1;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -30,16 +31,18 @@ public class MessageViewModel {
 
   public MessageViewModel(Message message) {
     this.message = message;
-
     // TODO exercice 1 : câbler le ViewModel.
     //
     // 1. Initialiser la propriété `texte` avec la valeur actuelle du modèle
-    //    (message.getTexte()).
+    // (message.getTexte()).
     // 2. Quand `texte` change, recopier la nouvelle valeur dans le modèle
-    //    (message.setTexte(...)) : c'est ce qui garde le modèle à jour.
+    // (message.setTexte(...)) : c'est ce qui garde le modèle à jour.
     // 3. Lier `apercu` (lecture seule pour la vue) à une version dérivée de
-    //    `texte` : le texte saisi précédé de la mention "Aperçu : ".
-    //    Astuce : Bindings.concat("Aperçu : ", texte).
+    // `texte` : le texte saisi précédé de la mention "Aperçu : ".
+    // Astuce : Bindings.concat("Aperçu : ", texte).
+    texte.set(message.getTexte());
+    texte.addListener((obs, oldVal, newVal) -> message.setTexte(newVal));
+    apercu.bind(Bindings.concat("Aperçu : ", texte));
   }
 
   public StringProperty texteProperty() {
